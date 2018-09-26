484f48fc62bd5df451c4ece0befc9082350a8c12
Amber1990Zhang <42762957+Amber1990Zhang@users.noreply.github.com>
2018-09-26 17:08:54 +0800

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,34 @@
+#Plot2
+
+#Download data and unzip
+if(!file.exists("./Exploratory_Data_Analysis")) 
+{dir.create("./Exploratory_Data_Analysis")}
+unzip(zipfile = "./Exploratory_Data_Analysis/Electric_Power_Consumption.zip")
+
+#Read data
+files <- file('./household_power_consumption.txt')
+week1data <-  read.table(text = grep("^[1,2]/2/2007",readLines(files),
+                         value=TRUE), sep = ';', 
+                         col.names = c("Date", "Time", "Global_active_power", 
+                         "Global_reactive_power", "Voltage", "Global_intensity",
+                         "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+                         na.strings = '?')
+names(week1data) <- tolower(names(week1data))
+
+#Convert date and time to specific format
+week1data$date <- as.Date(week1data$date, '%d/ %m/ %y')
+week1data$datetime <- as.POSIXct(paste(week1data$Date, week1data$Time))   
+                   
+#Open device
+if(!file.exists('plots')) {dir.create('plots')}
+png(filename = './plots/plot2.png', width = 480, height = 480, units = 'px')
+
+#Plot 
+Sys.setlocale(category = "LC_ALL", locale = "english")
+with(week1data, plot(datetime, global_active_power, xlab = '',
+                     ylab = 'Global Active Power (kilowatt)',
+                     type = 'l'))
+
+#Close device
+dev.off()
+
